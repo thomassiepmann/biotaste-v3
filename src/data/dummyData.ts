c84a310bbf6cf3bd0dbb4d7a0fd3acafcd361c61
Kistengenerator Dev <dev@kistengenerator.local>
2026-03-12 19:59:30 +0100

    fix: add dummyData.ts

---
+++ src/data/dummyData.ts
@@ -0,0 +1,8 @@
+as hast import { Charge } from '../types';
+
+export const DUMMY_CHARGES: Charge[] = [
+  { id: '1', product_id: '1', charge_code: 'ERD-2026-11', delivery_date: '2026-03-10' },
+  { id: '2', product_id: '2', charge_code: 'ZUC-2026-11', delivery_date: '2026-03-10' },
+];
+
+export const DUMMY_REWARDS: never[] = [];
